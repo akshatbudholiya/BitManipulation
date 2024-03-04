1f80c18c4b16ecc687f7e1f238f646321b7583ac
Akshat Narayan Budholiya <akshatnarayanb@gmail.com>
2024-03-04 12:52:12 +0530

    Create PowerOfFour.java

---
+++ PowerOfFour.java
@@ -0,0 +1,11 @@
+public class PowerOfFour {
+
+	public boolean isPowerOfFour(int n) {
+	     if(n > 0 && (n & (n - 1)) == 0 && (n & 0x55555555) != 0)
+	    	 
+	    	 return(true);  
+	       
+	       else 
+	    	   return(false);
+	    }
+}
